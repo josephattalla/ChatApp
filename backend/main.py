458221ec8d35034a696179ba2a56981838212b4c
Joseph Attalla <jojoatt10@gmail.com>
2025-06-06 15:14:29 -0400

    login endpoint w/jwt

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 from .controllers import socketEndpoint
+from .controllers import Login
 from .utils.ConnectionManager import ConnectionManager
 
 app = FastAPI()
@@ -10,6 +11,7 @@ manager = ConnectionManager()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(socketEndpoint.router, prefix="/ws")
+app.include_router(Login.router, prefix="/api")
 
 
 @app.get("/")
