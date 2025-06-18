830fdc812c4b538fcf9ac9edd21162311f89c463
Joseph Attalla <jojoatt10@gmail.com>
2025-06-18 16:36:14 -0400

    complete register endpoint with model d

--- backend/controllers/Register.py
+++ backend/controllers/Register.py
@@ -1,18 +1,48 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from pydantic.types import StringConstraints
+
+from ..utils.auth import UserInDB, fake_users_db, get_password_hash
 
 
 class RegisterUser(BaseModel):
-    username: str
-    password: str
+    username: Annotated[
+        str,
+        StringConstraints(
+            min_length=4,
+            max_length=20,
+        ),
+    ]
+    password: Annotated[
+        str,
+        StringConstraints(
+            min_length=8,
+            max_length=20,
+        ),
+    ]
 
 
 router = APIRouter()
 
 
+# TODO: implement with actual DB
 @router.post("/register", status_code=201)
 def register(registerData: RegisterUser):
-    # 1. validate username & password
-    # 2. hash password
-    # 3. add to DB
+    # check if username already in use
+    if registerData.username in fake_users_db:
+        raise HTTPException(status_code=409, detail="Username already in use.")
+
+    # hash password
+    hash_pass = get_password_hash(registerData.password)
+
+    # add to DB
+    user = UserInDB(
+        username=registerData.username,
+        hashed_password=hash_pass,
+        user_id=1,
+    )
+    fake_users_db[user.username] = user.model_dump()
+
     return
--- backend/utils/auth.py
+++ backend/utils/auth.py
@@ -17,7 +17,7 @@ fake_users_db = {
         # "full_name": "John Doe",
         # "email": "johndoe@example.com",
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "user_id": 1,
+        "user_id": 0,
         # "disabled": False,
     }
 }
