2e41be53ef77360f5777d5bb05733172f9de2c5f
ws-9 <93690318+ws-9@users.noreply.github.com>
2025-07-19 17:26:46 -0400

    add render messages

--- frontend/src/ChatContent.jsx
+++ frontend/src/ChatContent.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import useWebSocket from "react-use-websocket";
 import { AuthContext } from "./App";
 
 export default function ChatContent({ sessionId, selectedRoom }) {
   const { setAuthenticated, accessToken, userId } = useContext(AuthContext);
+  const [roomMessages, setRoomMessages] = useState([]);
   const WS_URL = `ws://localhost:8000/ws/${selectedRoom.room_id}`;
   const {
     sendJsonMessage,
@@ -21,19 +22,29 @@ export default function ChatContent({ sessionId, selectedRoom }) {
   );
 
   useEffect(function() {
-    if (lastJsonMessage) {
-      console.log(lastJsonMessage);
+    if (lastJsonMessage?.type === "room messages") {
+      setRoomMessages(lastJsonMessage.messages);
     }
   }, [lastJsonMessage]);
 
+  const renderedMessages = roomMessages.map(function({ username, chat }) {
+    return (
+      <article
+        key={chat.chat_id}
+        data-user-id={chat.user_id}
+        data-chat-id={chat.chat_id}
+      >
+        <h3>{username} ({chat.time})</h3>
+        <p>{chat.message}</p>
+      </article>
+    )
+  });
+
   return (
     <main className="chat-content">
-      <div className="messages-container">
-        <div className="message">Messages go here</div>
-        <div className="message">Messages go here</div>
-        <div className="message">Messages go here</div>
-        <div className="message">Messages go here</div>
-      </div>
+      <section className="messages-container">
+        {renderedMessages}
+      </section>
       <form action="" className="chatbox">
         <textarea
           name=""
